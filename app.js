4f5b4715dfbac81f256856a2012329858ab8405a
bnsnchz <bsanchez4488@gmail.com>
2018-06-14 22:51:54 -0700

    pushing what i could

--- app.js
+++ app.js
@@ -3,24 +3,40 @@ var clozeCard = require("./ClozeCard.js");
 var inquirer = require("inquirer");
 var counter = 0;
 
+inquirer.prompt([
+    {
+        type:"list",
+        name:"menu",
+        message:"What kind of game would you like to play?",
+        choices:["Basic","Cloze"],
+    }
+    ]).then(response=>{
 
-function basicFlash(){
-    if (counter < cardArr.length){
-    console.log("Card "+(counter+1));
-    console.log("Front: "+cardArr[counter].frontCard);
+    if(response.menu === "Basic"){
+    function basicFlash(){
+        if (counter < cardArr.length){
+        console.log("Card "+(counter+1));
+        console.log("Front: "+cardArr[counter].frontCard);
     
     
-    setTimeout(function(){
-        console.log("Back: "+cardArr[counter].backCard);
-    console.log("-----------------------------");
-        counter++
+        setTimeout(function(){
+            console.log("Back: "+cardArr[counter].backCard);
+            console.log("-----------------------------");
+            counter++
         setTimeout(function(){basicFlash();},3000);
-    },3000);
+        },3000);
     
+        }
+        else{
+            return;
+        }
     }
-    else{
-        return;
-    }
-}
 
 basicFlash();
+    }
+
+    if(response.menu === "Cloze"){
+        console.log("Comming Soon!");
+    }
+
+    });
